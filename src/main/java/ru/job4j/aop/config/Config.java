64d89d07cbe345bcf91d8bd4cfa9cc2868b4eab0
MasterPC <free_person@inbox.ru>
2023-09-27 22:09:21 +0300

    Концепция AOP [#505076]

---
+++ src/main/java/ru/job4j/aop/config/Config.java
@@ -0,0 +1,11 @@
+package ru.job4j.aop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@Configuration
+@ComponentScan(basePackages = "ru.job4j.aop")
+public class Config {
+}
